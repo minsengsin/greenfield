74869e0151cf3aff44e172399da129964217b465
Alex Chojnacki <thealex@umich.edu>
2018-02-19 20:02:29 -0500

    Added initial server files

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+let app = express();
+
+// Parse JSON (uniform resource locators)
+app.use(bodyParser.json());
+// Parse forms as well
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Declare static files
+app.use(express.static(__dirname + '/../client/build'));
+
+app.get('/hello', function(req, res) {
+  res.send('hello');
+});
+
+let port = process.env.PORT || 3001;
+
+app.listen(port, function() {
+  console.log(`listening on port ${port}`);
+});
+
