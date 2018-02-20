c22522a658e8dd1ea7bb0b75e21772286225f37d
BarkerAW83 <BarkerAW@gmail.com>
2018-02-20 14:52:59 -0500

    Header - First Commit - Issue 05

---
+++ client/src/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Header = function(props) {
+  return (
+    <div>
+      <h1>Team Lyly</h1>
+      <button href={props.userIsLoggedIn ? '/logout' : '/login'}>{props.userIsLoggedIn ? 'Logout!' : 'Login!'}</button>
+    </div>
+  );
+}
+
+// Header.defaultProps = {userIsLoggedIn: false};
+// Header.propTyes = {userIsLoggedIn: React.PropTypes.boolean};
+
+export default Header;
