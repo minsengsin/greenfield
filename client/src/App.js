d21264242c00d855c7fcd62d62f38a66aa02e0fb
BarkerAW83 <BarkerAW@gmail.com>
2018-02-20 20:08:11 -0500

    "TaskDetails initial commit"

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,18 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header.js';
+import sampleText from './SampleText.js';
+import TaskDetails from './TaskDetails.js'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
       <Header/>
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header> */}
+      <TaskDetails task={`
+      When I first brought my cat home from the Humane Society she was a mangy, pitiful animal. She was so thin that you could count her vertebrae just by looking at her. Apparently she was declawed by her previous owners, then abandoned or lost. Since she couldnt hunt, she nearly starved. Not only that, but she had an abscess on one hip. The vets at the Humane Society had drained it, but it was still scabby and without fur. She had a terrible cold, too. She was sneezing and sniffling and her meow was just a hoarse squeak. And shed lost half her tail somewhere. Instead of tapering gracefully, it had a bony knob at the end.
+      `}/>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
