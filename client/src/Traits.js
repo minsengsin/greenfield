ccddb7fdddacd25e95563ebd65bbdc708a4d5ce0
Alex Chojnacki <thealex@umich.edu>
2018-02-22 16:52:31 -0500

    Traits display information based on task in props

--- client/src/Traits.js
+++ client/src/Traits.js
@@ -1,20 +1,13 @@
-import React from 'react'
+import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Traits = function (props) {
-    return (
-        <div>
-            <ul>
-                <p> This task is scheduled for 
-                    {/* {props.task.date} */}
-                </p>
-                
-                <p> This task's location
-                    {/* {props.task.location} */}
-                </p>
-            </ul>
-        </div>
-    )
-}
+const Traits = function(props) {
+  return (
+    <ul>
+      <li>Date: {props.task.date}</li>
+      <li>Location: {props.task.location}</li>
+    </ul>
+  );
+};
 
 export default Traits;
