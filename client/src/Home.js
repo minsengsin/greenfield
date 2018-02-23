242b5ac6044b0301c8493ad47f5c4085dc920ff4
Alex Chojnacki <thealex@umich.edu>
2018-02-23 16:25:30 -0500

    Pushing to API

--- client/src/Home.js
+++ client/src/Home.js
@@ -46,12 +46,12 @@ class Home extends React.Component {
           <div className="four wide column">
             <TaskList tasks={this.state.tasks} />
           </div>
-          <script async defer src={`https://maps.googleapis.com/maps/api/js?key=AIzaSyC7t8S8b9WhSyAuT7Mr5VJaUhtDVDOUrV4&callback=initMap`}
+          <script async defer src={`https://maps.googleapis.com/maps/api/js?key=AIzaSyBH-6-MO2reXrAZ4fDQuzkOghyIBPkLyhE&callback=initMap`}
   type="text/javascript"></script>
           <div className="twelve wide column">
             <GoogleMaps
               isMarkerShown
-              googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyC7t8S8b9WhSyAuT7Mr5VJaUhtDVDOUrV4&v=3.exp&libraries=geometry,drawing,places"
+              googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBH-6-MO2reXrAZ4fDQuzkOghyIBPkLyhE&v=3.exp&libraries=geometry,drawing,places"
               loadingElement={<div style={{height: `100%`}} />}
               containerElement={<div style={{height: `100%`}} />}
               mapElement={<div style={{height: `100%`}} />}
