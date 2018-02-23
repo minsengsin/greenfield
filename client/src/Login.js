91b1ce68538ec0f086305cfd85748e8663de8b0c
Alex Chojnacki <thealex@umich.edu>
2018-02-22 19:28:35 -0500

    FIXED COLUMN WIDTH BUG
    Turns out that signup.css was loading an app-wide css adjustment
    restricting column width to 450px

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,45 +1,61 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import Signup from './Signup.js';
-import './signup.css'
+import './signup.css';
 
 class Login extends React.Component {
-	render() {
-		return (
-			<div className="ui container" style={{'paddingTop': '100px'}}>
-			<div className="ui middle aligned center aligned grid">
-				<div className="column">
-					<h2 className="ui blue image header">
-						<div className="content">
-							Log-in to VolunTinder
-							</div>
-					</h2>
-					<form className="ui large form" action="/login" method="post">
-						<div className="ui stacked segment">
-							<div className="field">
-								<div className="ui left icon input">
-								<label htmlFor="username"></label>
-                <input type="text" id="username" type="text" name="username" placeholder="Username"></input>
-								</div>
-							</div>
-							<div className="field">
-								<div className="ui left icon input">
-								<label htmlFor="password"></label>
-                 <input type="password" id="password" type="password" name="password" placeholder="Password"></input>
-								</div>
-							</div>
-							<input type="submit" value="Login" className="ui fluid large blue submit button"></input>
-						</div>
-						<div className="ui error message"></div>
-					</form>
-					<div className="ui message">
-					<Link className="ui fluid large blue submit button" to="/signup">New Volunteer? Register NOW!</Link>
-					</div>
-				</div>
-			</div>
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div className="ui container" style={{paddingTop: '100px'}}>
+        <div className="ui middle aligned center aligned grid">
+          <div className="column" style={{maxWidth: '450px'}}>
+            <h2 className="ui blue image header">
+              <div className="content">Log-in to VolunTinder</div>
+            </h2>
+            <form className="ui large form" action="/login" method="post">
+              <div className="ui stacked segment">
+                <div className="field">
+                  <div className="ui left icon input">
+                    <label htmlFor="username" />
+                    <input
+                      type="text"
+                      id="username"
+                      type="text"
+                      name="username"
+                      placeholder="Username"
+                    />
+                  </div>
+                </div>
+                <div className="field">
+                  <div className="ui left icon input">
+                    <label htmlFor="password" />
+                    <input
+                      type="password"
+                      id="password"
+                      type="password"
+                      name="password"
+                      placeholder="Password"
+                    />
+                  </div>
+                </div>
+                <input
+                  type="submit"
+                  value="Login"
+                  className="ui fluid large blue submit button"
+                />
+              </div>
+              <div className="ui error message" />
+            </form>
+            <div className="ui message">
+              <Link className="ui fluid large blue submit button" to="/signup">
+                New Volunteer? Register NOW!
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Login;
--- client/src/Signup.js
+++ client/src/Signup.js
@@ -1,45 +1,60 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import Login from './Login.js';
-import './signup.css'
+import './signup.css';
 
 class Signup extends React.Component {
-
   render() {
-		return (
-      <div className="ui container" style={{'paddingTop': '100px'}}>
-			<div className="ui middle aligned center aligned grid">
-				<div className="column">
-					<h2 className="ui blue image header">
-						<div className="content">
-							Sign-up for VolunTinder
-							</div>
-					</h2>
-					<form className="ui large form" action="/signup" method="post">
-						<div className="ui stacked segment">
-							<div className="field">
-								<div className="ui left icon input">
-								<label htmlFor="username"></label>
-                <input type="text" id="username" type="text" name="username" placeholder="Username"></input>
-								</div>
-							</div>
-							<div className="field">
-								<div className="ui left icon input">
-								<label htmlFor="password"></label>
-                 <input type="password" id="password" type="password" name="password" placeholder="Password"></input>
-								</div>
-							</div>
-							<input type="submit" value="Signup" className="ui fluid large blue submit button"></input>
-						</div>
-						<div className="ui error message"></div>
-					</form>
-					<div className="ui message">
-					<Link className="ui fluid large blue submit button" to="/login">Already registered? Login here!</Link>
-					</div>
-				</div>
-			</div>
+    return (
+      <div className="ui container" style={{paddingTop: '100px'}}>
+        <div className="ui middle aligned center aligned grid">
+          <div className="column" style={{maxWidth: '450px'}}>
+            <h2 className="ui blue image header">
+              <div className="content">Sign-up for VolunTinder</div>
+            </h2>
+            <form className="ui large form" action="/signup" method="post">
+              <div className="ui stacked segment">
+                <div className="field">
+                  <div className="ui left icon input">
+                    <label htmlFor="username" />
+                    <input
+                      type="text"
+                      id="username"
+                      type="text"
+                      name="username"
+                      placeholder="Username"
+                    />
+                  </div>
+                </div>
+                <div className="field">
+                  <div className="ui left icon input">
+                    <label htmlFor="password" />
+                    <input
+                      type="password"
+                      id="password"
+                      type="password"
+                      name="password"
+                      placeholder="Password"
+                    />
+                  </div>
+                </div>
+                <input
+                  type="submit"
+                  value="Signup"
+                  className="ui fluid large blue submit button"
+                />
+              </div>
+              <div className="ui error message" />
+            </form>
+            <div className="ui message">
+              <Link className="ui fluid large blue submit button" to="/login">
+                Already registered? Login here!
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
