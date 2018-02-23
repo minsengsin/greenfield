8bb1674a17b09016b8c624244a0c7ffc87a67db9
Alex Chojnacki <thealex@umich.edu>
2018-02-23 09:46:30 -0500

    Finalized routing in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header.js';
@@ -8,26 +8,23 @@ import Signup from './Signup.js';
 import mainPage from './mainPage.js';
 import OneTask from './oneTask.js';
 import sampleText from './SampleText.js';
-import TaskDetails from './TaskDetails.js'
+import TaskDetails from './TaskDetails.js';
 import Traits from './Traits.js';
 import Profile from './profile.js';
-import Home from './Home.js'
+import Home from './Home.js';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <div>
-          <Route path='/' component={Home} />
-          <Route path='/login' component={Login} />
-          <Route path='/signup' component={Signup} />
-          <Route path='/tasks/:taskId' component={TaskDetails} />
-          <Route path='/users/:username' component={Profile} />
-        </div>
+        <Route exact path="/" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/tasks/:taskId" component={TaskDetails} />
+        <Route path="/users/:username" component={Profile} />
       </div>
     );
   }
 }
-
 
 export default App;
