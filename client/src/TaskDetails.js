5e576a16a62fdbd6c705622905af99fd57e0846a
Alex Chojnacki <thealex@umich.edu>
2018-02-23 16:31:05 -0500

    Fixed taskdetails map page

--- client/src/TaskDetails.js
+++ client/src/TaskDetails.js
@@ -93,7 +93,7 @@ class TaskDetails extends React.Component {
           <div className="twelve wide column">
             <GoogleMaps
               isMarkerShown
-              googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+              googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBH-6-MO2reXrAZ4fDQuzkOghyIBPkLyhE&v=3.exp&libraries=geometry,drawing,places"
               loadingElement={<div style={{height: `100%`}} />}
               containerElement={<div style={{height: `100%`}} />}
               mapElement={<div style={{height: `100%`}} />}
