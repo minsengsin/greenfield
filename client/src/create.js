b8d5ae754d7d493038fb2412338bc8e5b12482a9
Eric Sin <eric@Erics-MacBook-Pro.local>
2018-03-06 12:28:21 -0500

    added functioning header to /Create route

--- client/src/create.js
+++ client/src/create.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import './signup.css';
 import axios from 'axios';
+import Header from './Header.js';
 
 class Create extends React.Component {
   constructor(props) {
@@ -14,7 +15,8 @@ class Create extends React.Component {
       location: '',
       title: '',
       description: '',
-      needed: 0
+      needed: 0,
+      username: this.props.match.params.username
     };
     this.handleTime = this.handleTime.bind(this);
     this.handleOrg = this.handleOrg.bind(this);
@@ -89,6 +91,8 @@ class Create extends React.Component {
 
   render() {
     return (
+    <div>
+      <Header name={this.state.username}/>
       <div className="ui container" style={{paddingTop: '100px'}}>
         <div className="ui middle aligned center aligned grid">
           <div className="column" style={{maxWidth: '450px'}}>
@@ -226,6 +230,7 @@ class Create extends React.Component {
           </div>
         </div>
       </div>
+    </div>
     );
   }
 }
